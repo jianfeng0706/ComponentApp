ce5a383c0731237d2ecb3ef2edea7872acd8522b
liujianfeng17 <liujianfeng17@jd.com>
2021-05-27 10:46:16 +0800

    添加业务组件依赖的Common Moudle

---
+++ Commonlib/src/main/java/com/jf/common/CommonUtil.java
@@ -0,0 +1,21 @@
+package com.jf.common;
+
+import androidx.annotation.NonNull;
+
+/**
+ * @ProjectName: ComponentApp
+ * @Package: com.jf.common
+ * @ClassName: CommonUtil
+ * @Description: java类作用描述
+ * @Author: liujianfeng17
+ * @CreateDate: 2021/5/27 10:43 AM
+ * @UpdateUser: 更新者：
+ * @UpdateDate: 2021/5/27 10:43 AM
+ * @UpdateRemark: 更新说明：
+ * @Version: 1.0
+ */
+public class CommonUtil {
+    public static void dealString(){
+
+    }
+}
